1d9e48afcd452dfa2ec04d3c9a20cfc96b4b270e
Tsilavina <159104790+TsilavinaZh@users.noreply.github.com>
2024-02-17 00:35:33 +0300

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# youtube_downloader
+
+Ce module charger des vidéos YouTube.
+
+## Installation
+
+Pour utiliser ce module, vous pouvez l'installer via la commande suivante :go get github.com/TsilavinaZh/youtube
+
+Voici un exemple d'utilisation du module pour télécharger une vidéo YouTube :
+
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/votre-utilisateur/youtube_downloader"
+)
+
+func main() {
+	videoURL := "https://www.youtube.com/watch?v=YOUR_VIDEO_ID"
+	err := youtube_downloader.Télécharger(videoURL)
+	if err != nil {
+		fmt.Printf("Erreur lors du téléchargement de la vidéo: %v\n", err)
+		return
+	}
+}Assurez-vous de remplacer YOUR_VIDEO_ID par l'ID de la vidéo YouTube que vous souhaitez télécharger.LicenseCe projet est sous licence MIT. Voir le fichier LICENSE pour plus de détails.Ce fichier README.md comprend toutes les informations nécessaires sur le module youtube_downloader, y compris l'installation, un exemple d'utilisation et des informations de licence.
---
+++ youtube.go
@@ -0,0 +1,41 @@
+package youtube_downloader
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/kkdai/youtube/v2"
+)
+
+func TÃ©lÃ©charger(videoURL string) error {
+	client := youtube.Client{}
+	videoID := extractVideoID(videoURL)
+	video, err := client.GetVideo(videoID)
+	if err != nil {
+		return fmt.Errorf("erreur lors de la rÃ©cupÃ©ration des dÃ©tails de la vidÃ©o: %v", err)
+	}
+	stream, _, err := client.GetStream(video, &video.Formats[0])
+	if err != nil {
+		return fmt.Errorf("erreur lors de la rÃ©cupÃ©ration de la meilleure qualitÃ© de la vidÃ©o: %v", err)
+	}
+	file, err := os.Create("video.mp4")
+	if err != nil {
+		return fmt.Errorf("erreur lors de la crÃ©ation du fichier pour enregistrer la vidÃ©o: %v", err)
+	}
+	defer file.Close()
+	_, err = stream.WriteTo(file)
+	if err != nil {
+		return fmt.Errorf("erreur lors de l'enregistrement de la vidÃ©o: %v", err)
+	}
+	fmt.Println("TÃ©lÃ©chargement terminÃ©!")
+	return nil
+}
+
+func extractVideoID(url string) string {
+	parts := strings.Split(url, "=")
+	if len(parts) > 1 {
+		return parts[1]
+	}
+	return ""
+}
